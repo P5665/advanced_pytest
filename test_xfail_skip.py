7946981d86541270947920c5e888dde38fb99c5e
Ben Peterson <benp5665@gmail.com>
2023-04-25 18:07:46 -0700

    Demo created custom marks and added them to pytest ini file.  Demo also includes using multiple decorators on a function

--- test_xfail_skip.py
+++ test_xfail_skip.py
@@ -1,3 +1,19 @@
+# 1 Demo  using xfail and skip
+# import pytest
+
+# def func_retval():
+#     some_int = 50
+#     return some_int
+
+# @pytest.mark.xfail
+# def test_func_one(input_value):
+#     assert func_retval() < input_value
+
+# @pytest.mark.skip
+# def test_func_two(input_value):
+#     assert func_retval() > input_value
+
+# 2 Demo using custom marks
 import pytest
 
 def func_retval():
@@ -8,6 +24,15 @@ def func_retval():
 def test_func_one(input_value):
     assert func_retval() < input_value
 
+@pytest.mark.markone
 @pytest.mark.skip
 def test_func_two(input_value):
+    assert func_retval() > input_value
+
+@pytest.mark.markone
+def test_func_three(input_value):
+    assert func_retval() == input_value
+
+@pytest.mark.marktwo
+def test_func_four(input_value):
     assert func_retval() > input_value
